16528f4d6f3cdc7289575db67e26f2f9828ffaa8
nakano_k <nakano7771@ymail.ne.jp>
2026-01-20 23:12:15 +0900

    fix: スコープチェックの柔軟化

--- backend/src/web/util/scope_checker.go
+++ backend/src/web/util/scope_checker.go
@@ -1,6 +1,14 @@
 package util
 
 func HasAnyScope(requireds []string, granteds []string) bool {
+	if len(requireds) == 0 {
+		return false
+	}
+
+	if len(granteds) == 0 {
+		return false
+	}
+
 	reqMap := make(map[string]struct{}, len(requireds))
 	for _, req := range requireds {
 		reqMap[req] = struct{}{}
