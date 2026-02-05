f49c53ff82e1b3a72d2065ffc34ba1babdd491ec
nakano_k <nakano7771@ymail.ne.jp>
2026-02-05 22:07:16 +0900

    fix: 細かい箇所の修正

--- frontend/components/RecommendedCookingItemSkeleton.vue
+++ frontend/components/RecommendedCookingItemSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-center items-center border border-gray-300 rounded-md p-4 min-h-48 sm:min-h-32 bg-white">
+    <div class="flex justify-center items-center border border-gray-300 rounded-md p-4 min-h-24 sm:min-h-32 bg-white">
         <div class="flex flex-col gap-3 items-center w-full">
             <div class="h-8 w-32 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
             <div class="h-6 w-64 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
