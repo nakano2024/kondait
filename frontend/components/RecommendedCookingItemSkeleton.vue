b6180d40cb65a8d2d070dff32f8d4b103b31ab85
nakano_k <nakano7771@ymail.ne.jp>
2026-02-06 00:05:02 +0900

    fix: カードのスタイルを修正

--- frontend/components/RecommendedCookingItemSkeleton.vue
+++ frontend/components/RecommendedCookingItemSkeleton.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="flex justify-center items-center border border-gray-300 rounded-md p-4 min-h-24 sm:min-h-32 bg-white">
-        <div class="flex flex-col gap-3 items-center w-full">
-            <div class="h-8 w-32 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
-            <div class="h-6 w-64 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
-            <div class="h-6 w-64 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
+    <div class="flex flex-col sm:flex-row items-center justify-center border border-gray-300 rounded-md p-4 min-h-24 sm:min-h-32 gap-3 bg-white">
+        <div class="flex flex-col items-center gap-2 text-center w-full sm:flex-1">
+            <div class="h-6 w-40 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
+            <div class="h-6 w-40 max-w-full rounded-md bg-gray-200 animate-pulse"></div>
+        </div>
+        <div class="flex items-center justify-center">
+            <div class="h-10 w-24 rounded-md bg-gray-200 animate-pulse"></div>
         </div>
     </div>
 </template>
