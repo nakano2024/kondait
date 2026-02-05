1c0f7b7e4862ed6f1717d0e5293c9192aa40d62e
nakano_k <nakano7771@ymail.ne.jp>
2026-02-05 23:57:15 +0900

    fix: カードのスタイルを修正

--- frontend/components/RecommendedCookingItemCard.vue
+++ frontend/components/RecommendedCookingItemCard.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="flex flex-col sm:flex-row items-center justify-center border border-gray-300 rounded-md p-4 min-h-24 sm:min-h-32 gap-4 bg-white">
-        <div class="flex flex-col items-center gap-3 text-center w-full sm:flex-1">
+    <div class="flex flex-col sm:flex-row items-center justify-center border border-gray-300 rounded-md p-4 min-h-24 sm:min-h-32 gap-3 bg-white">
+        <div class="flex flex-col items-center gap-2 text-center w-full sm:flex-1">
             <div class="font-bold text-xl">{{ name }}</div>
             <div>
                 <div v-if="isCooked">
-                    <span class="font-bold text-blue-600">{{ cookCount }}</span>回食べた
+                    <span class="font-bold text-green-700">{{ cookCount }}</span>回食べた
                 </div>
                 <div v-else>
                     <span class="font-bold text-red-700">まだ食べてない</span>
                 </div>
             </div>
             <div v-if="formattedLastCookedDate">
-                <span class="font-bold text-blue-500">{{ formattedLastCookedDate }}</span>が最後に食べた日
+                <span class="font-bold text-gray-600">{{ formattedLastCookedDate }}</span>が最後に食べた日
             </div>
         </div>
         <div class="flex items-center justify-center">
